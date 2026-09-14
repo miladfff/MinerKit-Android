5d186528012e5aa816980d25c2f02951b6dbace0
miladfff <miladfff@hotmail.com>
2026-09-15 01:59:17 +0330

    Add app/src/main/java/com/minerkit/android/MainActivity.kt

---
+++ app/src/main/java/com/minerkit/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.minerkit.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.minerkit.android.ui.MinerKitApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { MinerKitApp() }
+    }
+}
